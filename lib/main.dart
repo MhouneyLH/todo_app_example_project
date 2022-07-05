aa943fa7f0a5f12e7e2e865eb033e183907af232
Lucas Hünniger <lucas.huenniger02@gmail.com>
2022-07-05 19:37:10 +0200

    setup general firebase-implementation

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import './provider/todos.dart';
 import 'package:todo_application/provider/todos.dart';
 import 'pages/home_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   runApp(const MyApp());
 }
 
